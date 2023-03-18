a3df67e39aaf4b411763ed5d9060260382f1adfd
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 23:45:50 +0100

    Removing identity dashboard

--- src/renderer/src/views/Dashboard.vue
+++ src/renderer/src/views/Dashboard.vue
@@ -1,19 +1,7 @@
 <template>
-    <div class="mb-3">
-        <h3>Connections</h3>
-
-        <ConnectionList />
-    </div>
-
-    <div class="mb-3">
-        <h3>Identities</h3>
-
-        <IdentityList />
-    </div>
+    
 </template>
 
 <script setup lang="ts">
-import ConnectionList from '@renderer/components/ConnectionList.vue'
-import IdentityList from '@renderer/components/IdentityList.vue'
 
 </script>

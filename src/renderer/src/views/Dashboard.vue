db5655cd74291e835530eb5ec1666f4f3db05133
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 23:02:33 +0100

    Adding connections and identities to dashboard

--- src/renderer/src/views/Dashboard.vue
+++ src/renderer/src/views/Dashboard.vue
@@ -1,5 +1,19 @@
-<template></template>
+<template>
+    <div class="mb-3">
+        <h3>Connections</h3>
+
+        <ConnectionList />
+    </div>
+
+    <div class="mb-3">
+        <h3>Identities</h3>
+
+        <IdentityList />
+    </div>
+</template>
 
 <script setup lang="ts">
+import ConnectionList from '@renderer/components/ConnectionList.vue'
+import IdentityList from '@renderer/components/IdentityList.vue'
 
 </script>

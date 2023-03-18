326560401e235088dec3d35500db59f5afd57898
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 22:55:22 +0100

    Creating connections view

---
+++ src/renderer/src/views/Connections.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h3>Connections</h3>
+
+        <ConnectionList />
+        <ConnectionForm />
+    </div>
+</template>
+
+<script setup lang="ts">
+import ConnectionList from '@renderer/components/ConnectionList.vue'
+import ConnectionForm from '@renderer/components/ConnectionForm.vue'
+
+</script>

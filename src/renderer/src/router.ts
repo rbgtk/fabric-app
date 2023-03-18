9565028cc150593a6d8668d0aa455b0fee6fd749
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 22:53:46 +0100

    Adding connection and identity routes

--- src/renderer/src/router.ts
+++ src/renderer/src/router.ts
@@ -1,6 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = []
+import Home from '@renderer/views/Home.vue'
+import Connections from '@renderer/views/Connections.vue'
+import Identities from '@renderer/views/Identities.vue'
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/connections', component: Connections },
+    { path: '/identities', component: Identities }
+]
 
 export default createRouter({
     history: createWebHistory(),

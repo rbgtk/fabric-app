bc812e2ba5279c3d141094d778777f0b8a4f57df
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 22:56:02 +0100

    Layout modifications

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,7 +1,15 @@
 <template>
+  <Navigation class="mb-4" />
   
+  <div class="container">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+  import Navigation from './components/Navigation.vue'
 </script>
+
+<style>
+
+</style>

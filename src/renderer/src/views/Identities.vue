7ad4e7c970cc92c35d776a69e37cff880ba016a1
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 22:59:41 +0100

    Adding identity view and components

---
+++ src/renderer/src/views/Identities.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h3>Identities</h3>
+
+        <IdentityList />
+        <IdentityForm />
+    </div>
+</template>
+
+<script setup lang="ts">
+import IdentityList from '@renderer/components/IdentityList.vue'
+import IdentityForm from '@renderer/components/IdentityForm.vue'
+
+</script>

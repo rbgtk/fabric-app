4a6223d15e6a4f5d4e9d73365217dd9a0ce8bd57
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 22:56:29 +0100

    Adding connection and identity  navigation

---
+++ src/renderer/src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Fabric</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+                aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link active" to="home">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link active" to="connections">Connections</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>

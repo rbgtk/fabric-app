b1bb5f07cc53162e4d787902db63c921a1ccd823
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 20:59:45 +0100

    Adding bootstrap and fontawesom

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -4,6 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@fortawesome/fontawesome-free/js/all"
+
 createApp(App)
     .use(createPinia())
     .use(router)

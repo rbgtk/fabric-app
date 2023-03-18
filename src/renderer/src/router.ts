46be577a2e1d5ca9428bcaa544033777b8c53a0a
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 20:36:33 +0100

    Init router

---
+++ src/renderer/src/router.ts
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = []
+
+export default createRouter({
+    history: createWebHistory(),
+    linkActiveClass: 'active',
+    routes: routes
+})

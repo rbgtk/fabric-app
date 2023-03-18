78f5384f3a88d51f60f526620bb901d7ca756e2c
rbgtk <28433135+rbgtk@users.noreply.github.com>
2023-03-18 22:59:09 +0100

    Adding identity navigation

--- src/renderer/src/components/Navigation.vue
+++ src/renderer/src/components/Navigation.vue
@@ -14,6 +14,9 @@
                     <li class="nav-item">
                         <router-link class="nav-link active" to="connections">Connections</router-link>
                     </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link active" to="identities">Identities</router-link>
+                    </li>
                 </ul>
             </div>
         </div>
